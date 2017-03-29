4cd4d262145e5f1d3413f95d19545c9842ca3127
wangxr14 <wangxr1108@126.com>
2017-03-29 10:00:59 +0800

    Modify wsgi.py

--- oauth/wsgi.py
+++ oauth/wsgi.py
@@ -11,6 +11,17 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oauth.settings")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oauth.settings")
+
+from os.path import join,dirname,abspath
+ 
+PROJECT_DIR = dirname(dirname(abspath(__file__)))#3
+import sys # 4
+sys.path.insert(0,PROJECT_DIR) # 5
+ 
+os.environ["DJANGO_SETTINGS_MODULE"] = "blog.settings" # 7
 
 application = get_wsgi_application()
+
+
+ 
