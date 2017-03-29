eb6f51792d29cff81e0bf54b6efb3dd88e3c8c42
wangxr14 <wangxr1108@126.com>
2017-03-29 09:28:33 +0800

    Login with github

---
+++ oauth/view.py
@@ -0,0 +1,41 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+ 
+import urllib
+import urllib2
+import json
+ 
+client_id = '59e20ba35722b5a2a905'
+client_secret = '1d2bc69a9c54627f8e0c45cb8e2d63a5d3ca2c5d'
+code = ''
+ 
+def login(request):
+    context          = {}
+    context['login'] = 'Click and login now!'
+    return render(request, 'login.html', context)
+	
+def loginWithGithub(request):
+	code = request.GET.get('code','')
+	requrl = 'https://github.com/login/oauth/access_token'
+	data = {'client_id':client_id,'client_secret':client_secret,'code':code}
+	data_urlencode = urllib.urlencode(data)
+	req = urllib2.Request(url = requrl,data =data_urlencode)
+	res_data = urllib2.urlopen(req)
+	res = res_data.read()
+	tmp = res.split('&')
+	tokeninfo = tmp[0].split('=')
+	access_token = tokeninfo[1]
+	
+	tokenurl = 'https://api.github.com/user?access_token='+access_token
+	tokenreq = urllib2.Request(tokenurl)
+	res_data = urllib2.urlopen(tokenreq)
+	res = res_data.read()
+	emailaddr_json = json.loads(res)
+	email_addr = emailaddr_json['email']
+
+	return render(request, 'redirect_back.html',{'email_addr':email_addr})
+
+def renderSearchPage(request):
+	domain = request.GET.get('domain','')
+	
+	return render(request, 'searchPage.html',{'domain':domain})
